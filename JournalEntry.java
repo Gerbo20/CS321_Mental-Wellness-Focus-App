1b938d2929802cc31c2d0c6f771c75249934bf7f
Autonomous-jpg <christopher.e.baires@gmail.com>
2025-11-23 19:01:57 -0500

    JournalEntry.java

--- JournalEntry.java
+++ JournalEntry.java
@@ -1,24 +1,44 @@
 import java.time.*;
 
 public class JournalEntry {
+    private final String title;
+    private final String mood;
     private final LocalDate date;
     private final String entry;
     
-    public JournalEntry(String entry) {
+    // Constructor for JournalEntry
+    public JournalEntry(String title, String mood, String entry) {
+        this.title = title;
+        this.mood = mood;
         this.date = LocalDate.now();
         this.entry = entry;
     }
 
+    // Gets the current date when entry is created
     public LocalDate getDate() {
         return date;
     }
 
+    // Gets the title of the entry
+    public String getTitle() {
+        return title;
+    }
+
+    // Gets the chosen mood for the entry (Selection tab available in GUI)
+    public String getMood() {
+        return mood;
+    }
+
+    // The entry itself
     public String getEntry() {
         return entry;
     }
 
+    // String representation of how the entry will be displayed
     @Override
     public String toString() {
-        return "{" + getDate() +"}\n" + "==============================\n\n" + entry + "\n\n==============================\n";
+        return "{" + getDate() +"} [" + mood + "]\n" +
+        "Title: " + title + "\n" +
+        "==============================\n\n" + "\"" + entry + "\"" + "\n\n==============================\n";
     }
 }
